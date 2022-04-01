a9250e3c49c70a71339d3dd0f52ea70532351211
Gopika2651 <101546899+Gopika2651@users.noreply.github.com>
2022-04-01 17:29:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine("Original string: " + str1);
+            Console.WriteLine("Position of the word 'fox' in the said string: " + test(str1, "fox"));
+        }
+        public static int test(string text, string word)
+        {
+            return Array.IndexOf(text.Split(' '), word) + 1;
+        }
+    }
+}
